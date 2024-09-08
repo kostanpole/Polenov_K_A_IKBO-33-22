01d038136ee5e49e874c2b25b42d4709356285aa
kostanpole <77924505+kostanpole@users.noreply.github.com>
2024-09-08 22:01:32 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-document.querySelector('.singkapvo-gesionalem');// Выбор элемента на странице с классом .singkapvo-gesionalem
-onmousemove = function (e) { // Добавление события onmousemove
-    var x = e.pageX - e.target.offsetLeft;  // Получение координат указателя мыши относительно выбранного элемента
-    var y = e.pageY - e.target.offsetTop;
-    e.target.style.setProperty('--x', x + 'px'); // Установка пользовательских свойств --x и --y для элемента
-    e.target.style.setProperty('--y', y + 'px');
-};
+
